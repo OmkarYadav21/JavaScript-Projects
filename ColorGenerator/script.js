b7ca72feea399da3b4f887a62c4a2f963304cb0f
omkar2143 <omkaryadav301020@gmail.com>
2023-11-22 15:49:58 +0530

    Added ColorGenerator to the repository

---
+++ ColorGenerator/script.js
@@ -0,0 +1,11 @@
+const getColor=()=>{
+    const random=Math.floor(Math.random()*16777215);
+    const randomCode="#"+random.toString(16);
+    console.log(randomCode);
+    document.body.style.backgroundColor=randomCode;
+    document.getElementById('color-code').innerText=randomCode;
+    navigator.clipboard.writeText(randomCode);
+}
+document.getElementById('btn').addEventListener('click',getColor);
+
+getColor();
